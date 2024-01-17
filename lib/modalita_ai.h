50ad9307d446ed5a08c3880ba1381891e267e967
Alessandro Derevytskyy <899454@stud.unive.it>
2024-01-17 18:25:34 +0100

    revisione algoritmo sulla base dei nuovi controlli

--- lib/modalita_ai.h
+++ lib/modalita_ai.h
@@ -4,6 +4,6 @@
 #include "struttura_dati.h"
 
 // Restituisci la prima tessera con una corrispondenza
-estremo_t *first_match(matrice_t *piano_gioco, vect_t *mano_giocatore, comb_t *risultato);
+tessera_t *first_match(matrice_t *piano_gioco, vect_t *mano_giocatore, comb_t *risultato);
 
 #endif
--- src/modalita_ai.c
+++ src/modalita_ai.c
@@ -2,28 +2,26 @@
 
 #include "tessera.h"
 
-estremo_t *first_match(matrice_t *piano_gioco, vect_t *mano_giocatore, comb_t *risultato) {
-    unsigned int corrispondenza;
-    // Per ciascuna tessera nella mano del giocatore
-    for (size_t tessera = 0; tessera < mano_giocatore->dimensione; tessera++) {
-        // Per ciascuna riga del piano di gioco
-        for (size_t riga = 0; riga < piano_gioco->righe; riga++) {
-            // Calcola la prima ed ultima posizione utile
-            unsigned int inizio = prima_posizione(piano_gioco, riga);
-            unsigned int fine = ultima_posizione(piano_gioco, riga);
-            // Scorri tutte le posizioni utili
-            for (size_t colonna = inizio; colonna <= fine; colonna++) {
-                // Nella posizione corrente
-                coord_t corrente = {riga, colonna};
-                if (/* e' possibile posizionarla orizzontale/verticale + ordinata/girata? */) {
-                    // Modifica i parametri necessari a posizionare la tessera
-                    risultato->posizione = corrente;
-                    risultato->rotazione = corrispondenza - 1;
-                    // Ritorna l'indice della tessera trovata
-                    return elemento_ad_indice(mano_giocatore, tessera);
-                }
+tessera_t *first_match(matrice_t *piano_gioco, vect_t *mano_giocatore, comb_t *risultato) {
+    do {
+        // Calcola tutte le coordinate in cui e' possibile posizionare una tessera
+        vect_t *coordinate = calcola_coordinate(piano_gioco, risultato->orientamento);
+        // Per ciascuna tessera normale nella mano del giocatore
+        for (size_t i=0; i < mano_giocatore->dimensione - risultato->speciali; i++) {
+            // Ricava l'indirizzo della tessera da controllare
+            tessera_t *tessera = elemento_ad_indice(mano_giocatore, tessera);
+            // Se una delle posizioni fornite permette l'inserimento della tessera
+            if ((risultato->rotazione = mossa_legale(piano_gioco, coordinate, tessera, risultato->orientamento)) != 0) {
+                // Ritorna l'indice della tessera trovata
+                return elemento_ad_indice(mano_giocatore, tessera);
             }
         }
+    // Ripeti questo per le posizioni in verticale (0) e in orizzontale (1)
+    } while(!risultato->orientamento++);
+    // Nel caso in cui fossero rimaste tessere speciali
+    if(risultato->speciali > 0) {
+        // Posiziona la prima tessera speciale disponibile
+        return elemento_ad_indice(mano_giocatore, mano_giocatore->dimensione - (risultato->speciali);
     }
     // Non ho trovato nessuna tessera da posizionare
     return NULL;
